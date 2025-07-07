92fab89b616d837f0b8ac41389b67abebc487648
Si7C-08 <Si7C-08@i7C-85>
2025-07-07 09:55:05 +0900

    MainConsole 수정!

--- src/console/MainConsole.java
+++ src/console/MainConsole.java
@@ -1,7 +1,15 @@
 package console;
 
+import java.util.Scanner;
+
 public class MainConsole extends Thread {
+	Scanner sc = new Scanner(System.in);
+	
 	@Override
 	public void run() {
+		while (true) {
+			System.out.println("> ");
+			sc.nextLine();
+		}
 	}
 }
